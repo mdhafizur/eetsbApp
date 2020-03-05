6ec9b2f72505577dd6727095b4ee51185094d697
mdhafizur <hafizur.upm@gmail.com>
2020-03-05 13:47:28 +0800

    'first'

---
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.demo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+
+
+    }
+
+
